997791fd03bd31dcc5f81bcf4c08530c7dfe326b
mungho <mung.hokha@gmail.com>
2025-05-23 20:49:42 +0700

    Bai5 CRUD san pham

---
+++ src/Bai5_access_modifier/ProductManagement/Product.java
@@ -0,0 +1,37 @@
+package Bai5_access_modifier.ProductManagement;
+
+public class Product {
+    private String id;
+    private String name;
+    private double price;
+
+    public Product(String id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Product() {
+        //initial a non object value
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
